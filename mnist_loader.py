880124facde43fb4c366586bf310fcdce93ebdf9
omarsar <ellfae@gmail.com>
2016-01-22 14:52:30 +0800

    added some extra descriptions

--- mnist_loader.py
+++ mnist_loader.py
@@ -66,11 +66,17 @@ def load_data_wrapper():
     turn out to be the most convenient for use in our neural network
     code."""
     tr_d, va_d, te_d = load_data()
+
+    #this makes sure to turn the array from horizantal structure to vertical array structure.
     training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
     training_results = [vectorized_result(y) for y in tr_d[1]]
     training_data = zip(training_inputs, training_results)
+
+    #FOR THE  validation data the format remains the same for the classificaiton value vector
     validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
     validation_data = zip(validation_inputs, va_d[1])
+
+
     test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
     test_data = zip(test_inputs, te_d[1])
     return (training_data, validation_data, test_data)
